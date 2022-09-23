d436a7fa0b0a4c1e838db6fdc537e96a105d57aa
Vi Đặng <danglongvipro@gmail.com>
2022-09-23 08:17:22 +0700

    update cloudinary plugin

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -17,6 +17,21 @@ module.exports = ({ env }) => {
         },
       },
     },
+    upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_NAME'),
+          api_key: env('CLOUDINARY_KEY'),
+          api_secret: env('CLOUDINARY_SECRET'),
+        },
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
+        },
+      },
+    },
     ckeditor: {
       enabled: true,
       config: {
