dad612c5aae304328bfe286b23c0ca3dacc86b6d
Vi Đặng <danglongvipro@gmail.com>
2022-10-03 21:06:43 +0700

    update profile nav

--- frontend/components/profile/profileNav.tsx
+++ frontend/components/profile/profileNav.tsx
@@ -1,6 +1,38 @@
+import { useEffect, useRef } from "react"
+
 const ProfileNav = () => {
+  const ref = useRef<HTMLUListElement>(null)
+
+  useEffect(() => {
+    const onScroll = () => {
+      const position = window.scrollY - 570
+      const navbarLinks = ref.current?.querySelectorAll(".scrollto") ?? []
+      // remove all active class
+      navbarLinks.forEach((navbarlink) => {
+        navbarlink.classList.remove("active")
+      })
+      // add active class to first item
+      let actived = false
+      navbarLinks.forEach((navbarlink) => {
+        if (actived || !navbarlink.hash) return
+        const section = document.querySelector<HTMLElement>(navbarlink.hash)
+        if (!section) return
+        if (
+          position >= section.offsetTop - 50 &&
+          position <= section.offsetTop + section.offsetHeight + 200
+        ) {
+          navbarlink.classList.add("active")
+          actived = true
+        }
+      })
+    }
+    onScroll()
+    document.addEventListener("scroll", onScroll)
+    return () => document.removeEventListener("scroll", onScroll)
+  }, [])
+
   return (
-    <ul className="nav nav-pills flex-column">
+    <ul className="profile-nav nav nav-pills flex-column" ref={ref}>
       <li className="nav-item px-3">
         <a href="#personal" className="nav-link scrollto active">
           <i className="fas fa-fw fa-info-circle me-1" aria-hidden="true" />
