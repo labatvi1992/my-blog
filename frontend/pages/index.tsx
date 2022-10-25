efe235038bf9fdae2e91b6ff0486ae31ed0b55aa
Vi Đặng <danglongvipro@gmail.com>
2022-10-25 10:09:04 +0700

    update homepage

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,7 +1,27 @@
 import { fetchAPI } from "@/common/helpers/api"
 import { serverSideTranslations } from "next-i18next/serverSideTranslations"
+import { useGlobalContext } from "@/components/common/globalContext"
+import { THomePageProp } from "@/common/types/THomePage"
+import FeaturedBlog from "@/components/advance/home/featuredBlog"
+import FeaturedGallery from "@/components/advance/home/featuredGallery"
+import Subscribe from "@/components/advance/home/subscribe"
+import Welcome from "@/components/advance/home/welcome"
+import Layout from "@/components/common/layout"
+import Seo from "@/components/common/seo"
 
-export { default } from "@/components/advance/home"
+const Home = (prop: THomePageProp) => {
+  const global = useGlobalContext()
+  const { homepage, featuredGallery, featuredBlog } = prop || {}
+  return (
+    <Layout global={global}>
+      <Seo seo={homepage?.attributes?.seo} />
+      <Welcome data={homepage?.attributes?.welcome} />
+      <FeaturedGallery data={featuredGallery} />
+      <FeaturedBlog data={featuredBlog} />
+      <Subscribe />
+    </Layout>
+  )
+}
 
 export const getServerSideProps = async ({ locale }) => {
   const [homepageRes, featuredGalleryRes, featuredBlogRes] = await Promise.all([
@@ -38,3 +58,5 @@ export const getServerSideProps = async ({ locale }) => {
     },
   }
 }
+
+export default Home
